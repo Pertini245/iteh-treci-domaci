ed10368f33cb9b988b864c5cde1e5bd83cfa5a46
Pertini245 <petariristic@gmail.com>
2022-02-04 15:23:18 +0100

    Prikaz svih laptopova

--- src/components/PrikazLaptopova.js
+++ src/components/PrikazLaptopova.js
@@ -1,10 +1,27 @@
-function PrikazLaptopova() {
 
 
+function PrikazLaptopova(props) {
+
+    var laptopovi = props.laptopovi;
+
+    function obrisiLaptop(id) {
+
+    }
 
     return (
-        <div className="prikaz">
-
+        <div className="svi-laptopovi-prikaz">
+            {
+                laptopovi.map((laptop) => (
+                    <div className="laptop-prikaz" key={laptop.id}>
+                        <h2 id="laptop-model">{laptop.model}</h2>
+                        <img src={laptop.slika}></img>
+                        <h3 className="laptop-osobine">Procesor: {laptop.procesor}</h3>
+                        <h3 className="laptop-osobine">Grafička karta: {laptop.graficka}</h3>
+                        <h3 className="laptop-osobine">Cena: {laptop.cena} RSD</h3>
+                        <button onClick={() => obrisiLaptop(laptop.id)} id="dugme-obrisi">Obrisi</button>
+                    </div>
+                ))
+            }
         </div>
     );
 }
