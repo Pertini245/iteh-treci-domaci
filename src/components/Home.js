da2cbbb8d98af9cfc84be6b8f4e60215809f15db
Pertini245 <petariristic@gmail.com>
2022-02-04 15:11:18 +0100

    Home.js, PrikazLaptopova.js, prenos laptopova preko propsa, rutiranje u App.js podešeno

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import PrikazLaptopova from "./PrikazLaptopova";
+
+
+function Home() {
+
+    const [laptopovi, setLaptopovi] = useState([
+        { id: 1, model: 'RAZER Blade 15', procesor: 'Intel® Core™ i7-11800H', graficka: 'GeForce RTX 3060', slika: 'https://img.gigatron.rs/img/products/large/image61ee661c85be1.png', cena: 329999 },
+        { id: 2, model: 'HP Spectre x360', procesor: 'Intel® Core™ i7-1065G7', graficka: 'Iris Plus Graphics G7', slika: 'https://img.gigatron.rs/img/products/large/image5f7f1cdd52134.png', cena: 240999 },
+        { id: 1, model: 'ACER Predator Helios 300', procesor: 'Intel® Core™ i7-11800H ', graficka: 'GeForce RTX 3070', slika: 'https://img.gigatron.rs/img/products/large/image61cc2f1acd484.png', cena: 269999 }
+    ]);
+
+
+    return (
+        <div className="home">
+            <h1>Laptop E-commerce</h1>
+            <PrikazLaptopova laptopovi={laptopovi}></PrikazLaptopova>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/PrikazLaptopova.js
@@ -0,0 +1,12 @@
+function PrikazLaptopova() {
+
+
+
+    return (
+        <div className="prikaz">
+
+        </div>
+    );
+}
+
+export default PrikazLaptopova;
